14c2d241118d8fbdd4da369230b1f131655ce5b8
TJ <michael.btj@hotmail.com>
2022-01-17 22:54:47 -0500

    add dark

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight} />
         <TextRepeaterButton />
       </section>
     </div>
